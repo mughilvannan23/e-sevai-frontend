1df108e9d9f808901fd685308887131ae2937d96
sudhar-dev-02 <sudharsanp300@gmail.com>
2026-04-15 17:54:39 +0530

    /

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:"/sonagiri-bookinengine-frontend/" ,
+  base:"/e-sevai-frontend/" ,
   plugins: [react()],
 })
